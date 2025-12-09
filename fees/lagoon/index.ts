5773e3e61ac62130baf35786ce0b68e102c8eb2d
rohannero <rohanzarathustra@gmail.com>
2025-12-09 17:12:33 -0500

    alt lagoon fees/rev tracking

--- fees/lagoon/index.ts
+++ fees/lagoon/index.ts
@@ -1,118 +1,189 @@
-import { METRIC } from "../../helpers/metrics";
-import { Adapter, BaseAdapterChainConfig, FetchOptions, FetchResultV2 } from "../../adapters/types";
-import { InfraConfigs } from "./config";
-
-// docs: https://docs.lagoon.finance/vault/fees
-// Lagoon allows curators to deploy vaults - where users can deposit and earn yields
-// curators can config and share of yield from performance and management fees
-// on top of that, Lagoon earn 10% from what curators earn
-
-const LAGOON_PROTOCOL_FEES_RATE = 0.1 // 10%
-
-const Abis = {
-  ProxyDeployedEvent: 'event ProxyDeployed(address proxy, address deployer)',
-  convertToAssets: 'function convertToAssets(uint256) view returns (uint256)',
-  feeRates: 'function feeRates() view returns (uint256 managementRate, uint256 performanceRate)',
-}
-
-async function fetch(options: FetchOptions): Promise<FetchResultV2> {
-  const dailyFees = options.createBalances()
-  const dailyRevenue = options.createBalances()
-  const dailyProtocolRevenue = options.createBalances()
-  const dailySupplySideRevenue = options.createBalances()
-
-  let vaults: Array<string> = InfraConfigs[options.chain].vaults;
-  for (const factory of InfraConfigs[options.chain].factories) {
-    const events = await options.getLogs({
-      eventAbi: Abis.ProxyDeployedEvent,
-      target: factory.address,
-      fromBlock: factory.fromBlock,
-    });
-    vaults = vaults.concat(events.map((e: any) => e.proxy))
-  }
-
-  const assets = await options.api.multiCall({ abi: 'address:asset', calls: vaults, permitFailure: true })
-  const balances = await options.api.multiCall({ abi: 'uint256:totalAssets', calls: vaults, permitFailure: true })
-  const feeRates = await options.api.multiCall({ abi: Abis.feeRates, calls: vaults, permitFailure: true })
-  const convertCalls = vaults.map((vault: string) => {
-    return {
-      target: vault,
-      params: [String(1e18)],
-    }
-  })
-  const cumulativeIndexBefore = await options.fromApi.multiCall({ abi: Abis.convertToAssets, calls: convertCalls, permitFailure: true, })
-  const cumulativeIndexAfter = await options.toApi.multiCall({ abi: Abis.convertToAssets, calls: convertCalls, permitFailure: true, })
-
-  for (let i = 0; i < vaults.length; i++) {
-    if (assets[i] && balances[i] && cumulativeIndexBefore[i] && cumulativeIndexAfter[i]) {
-      const cumulativeYield = (BigInt(cumulativeIndexAfter[i]) - BigInt(cumulativeIndexBefore[i])) * BigInt(balances[i]) / BigInt(1e18)
-      
-      const managementFeeRate = Number(feeRates[i] ? Number(feeRates[i].managementRate) / 1e4 : 0)
-      const performanceFeeRate = Number(feeRates[i] ? Number(feeRates[i].performanceRate) / 1e4 : 0)
-      
-      const performanceFees = Number(cumulativeYield) * performanceFeeRate
-      
-      const oneYear = 365 * 24 * 3600
-      const timeframe = options.toTimestamp - options.fromTimestamp
-      const managementFees = Number(balances[i]) * Number(managementFeeRate) * timeframe / oneYear
-
-      const supplySideYields = Number(cumulativeYield) - performanceFees
-      
-      dailyFees.add(assets[i], supplySideYields, METRIC.ASSETS_YIELDS);
-      dailyFees.add(assets[i], performanceFees, METRIC.PERFORMANCE_FEES);
-      dailyFees.add(assets[i], managementFees, METRIC.MANAGEMENT_FEES);
-
-      dailySupplySideRevenue.add(assets[i], supplySideYields, METRIC.ASSETS_YIELDS);
-      
-      dailyRevenue.add(assets[i], performanceFees, METRIC.PERFORMANCE_FEES);
-      dailyRevenue.add(assets[i], managementFees, METRIC.MANAGEMENT_FEES);
-
-      dailyProtocolRevenue.add(assets[i], Number(performanceFees) * LAGOON_PROTOCOL_FEES_RATE, METRIC.PERFORMANCE_FEES);
-      dailyProtocolRevenue.add(assets[i], Number(managementFees) * LAGOON_PROTOCOL_FEES_RATE, METRIC.MANAGEMENT_FEES);
-    }
-  }
-  
-  return {
-    dailyFees,
-    dailyRevenue,
-    dailySupplySideRevenue,
-    dailyProtocolRevenue,
-  }
-}
-
-const adapter: Adapter = {
-  version: 2,
-  adapter: {},
-  methodology: {
-    Fees: 'Total yield generated from supplied assets.',
-    Revenue: 'Amount of performance and management fees to vault deployers and Lagoon protocol.',
-    SupplySideRevenue: 'Amount of yields distributed to vault suppliers.',
-    ProtocolRevenue: '10% of performance and management fees collected by Lagoon protocol.',
-  },
-  breakdownMethodology: {
-    Fees: {
-      [METRIC.ASSETS_YIELDS]: 'Amount of yields after performance and management fees cut.',
-      [METRIC.MANAGEMENT_FEES]: 'Management fees share to vault deployers and Lagoon protocol.',
-      [METRIC.PERFORMANCE_FEES]: 'Performance fees share to vault deployers and Lagoon protocol.',
-    },
-    Revenue: {
-      [METRIC.MANAGEMENT_FEES]: 'Management fees share to vault deployers and Lagoon protocol.',
-      [METRIC.PERFORMANCE_FEES]: 'Performance fees share to vault deployers and Lagoon protocol.',
-    },
-    ProtocolRevenue: {
-      [METRIC.MANAGEMENT_FEES]: '10% of management fees share to Lagoon protocol.',
-      [METRIC.PERFORMANCE_FEES]: '10% of performance fees share to Lagoon protocol.',
-    },
-    SupplySideRevenue: {
-      [METRIC.ASSETS_YIELDS]: 'Amount of yields after performance and management fees cut to suppliers.',
-    },
-  }
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { graph } from "@defillama/sdk";
+// Chains that have different subgraph names
+const chainToSubgraphName: Record<string, string> = {
+  [CHAIN.ETHEREUM]: "mainnet",
+  [CHAIN.AVAX]: "avalanche",
+  [CHAIN.HYPERLIQUID]: "hyperevm",
+  [CHAIN.UNICHAIN]: "unichain-mainnet",
+  [CHAIN.WC]: "worldchain-mainnet",
 };
 
-for (const [chain, config] of Object.entries(InfraConfigs)) {
-  (adapter.adapter as BaseAdapterChainConfig)[chain] = {
-    fetch,
-    start: config.start,
+const getSubgraphUrl = (chain: string) => {
+  const subgraphChain = chainToSubgraphName[chain] || chain;
+  return `https://api.goldsky.com/api/public/project_cmbrqvox367cy01y96gi91bis/subgraphs/lagoon-${subgraphChain}-vault/prod/gn`;
+};
+
+const abis = {
+  getRolesStorage: "function getRolesStorage() view returns (address whitelistManager, address feeReceiver, address safe, address feeRegistry, address valuationManager)",
+  protocolFeeReceiver: "function protocolFeeReceiver() view returns (address)",
+  asset: "function asset() view returns (address)",
+  convertToAssets: "function convertToAssets(uint256 shares) view returns (uint256)"
+}
+
+const fetch = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances()
+  const dailyRevenue = options.createBalances()
+  const subgraphUrl = getSubgraphUrl(options.chain);
+
+  const settleRedeemsQuery = await graph.request(subgraphUrl, `{
+        settleRedeems(
+            where: { blockTimestamp_gte: ${Math.floor(options.fromTimestamp)}, blockTimestamp_lte: ${Math.floor(options.toTimestamp)} }
+        ) {
+            vault
+            transactionHash
+        }
+    }`);
+  if (!settleRedeemsQuery.settleRedeems || settleRedeemsQuery.settleRedeems.length === 0) {
+    return { dailyFees, dailyRevenue };
+  }
+
+  const uniqueVaults = [...new Set(settleRedeemsQuery.settleRedeems.map((s: any) => s.vault))];
+  if (uniqueVaults.length === 0) {
+    return { dailyFees, dailyRevenue };
+  }
+
+  const [roleStorages, assets] = await Promise.all([
+    options.api.multiCall({
+      abi: abis.getRolesStorage,
+      calls: uniqueVaults as string[],
+    }),
+    options.api.multiCall({
+      abi: abis.asset,
+      calls: uniqueVaults as string[],
+    })
+  ]);
+  // Get protocolFeeReceiver from the first vault's feeRegistry (same for all vaults)
+  const feeRegistry = roleStorages[0].feeRegistry;
+  const protocolFeeReceiver = (await options.api.call({
+    target: feeRegistry,
+    abi: abis.protocolFeeReceiver,
+  })).toLowerCase();
+
+  // Map vault addresses to feeReceiver and asset
+  const vaultConfigs = new Map(
+    (uniqueVaults as string[]).map((vault: string, i: number) => [
+      vault.toLowerCase(),
+      {
+        feeReceiver: roleStorages[i].feeReceiver.toLowerCase(),
+        asset: assets[i],
+      }
+    ])
+  );
+
+  // Get all fee transfers from settlement transactions in one query
+  const allTxHashes = settleRedeemsQuery.settleRedeems
+    .map((s: any) => `"${s.transactionHash.toLowerCase()}"`)
+    .join(',');
+  const feeTransfersQuery = await graph.request(subgraphUrl, `{
+        transfers(
+            where: { 
+                transactionHash_in: [${allTxHashes}], 
+                from: "0x0000000000000000000000000000000000000000"
+            }
+        ) {
+            vault
+            to
+            value
+            transactionHash
+        }
+    }`);
+  if (!feeTransfersQuery.transfers || feeTransfersQuery.transfers.length === 0) {
+    return { dailyFees, dailyRevenue };
+  }
+
+  const assetAmounts = await options.api.multiCall({
+    abi: abis.convertToAssets,
+    calls: feeTransfersQuery.transfers.map((transfer: any) => ({
+      target: transfer.vault,
+      params: [transfer.value]
+    }))
+  });
+
+  for (let i = 0; i < feeTransfersQuery.transfers.length; i++) {
+    const assetAmount = assetAmounts[i];
+    const toAddress = feeTransfersQuery.transfers[i].to.toLowerCase();
+    const vaultConfig = vaultConfigs.get(feeTransfersQuery.transfers[i].vault.toLowerCase());
+    if (!vaultConfig) continue;
+
+    if (toAddress === vaultConfig.feeReceiver) {
+      dailyFees.add(vaultConfig.asset, assetAmount);
+    } else if (toAddress === protocolFeeReceiver) {
+      dailyRevenue.add(vaultConfig.asset, assetAmount);
+    }
+  }
+
+  return { dailyFees, dailyRevenue }
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.ARBITRUM]: {
+      fetch,
+      start: "4-8-2025",
+    },
+    [CHAIN.AVAX]: {
+      fetch,
+      start: "7-17-2025",
+    },
+    [CHAIN.BASE]: {
+      fetch,
+      start: "5-1-2025",
+    },
+    [CHAIN.ETHEREUM]: {
+      fetch,
+      start: "4-8-2025",
+    },
+    [CHAIN.HYPERLIQUID]: {
+      fetch,
+      start: "9-8-2025",
+    },
+    [CHAIN.KATANA]: {
+      fetch,
+      start: "7-4-2025",
+    },
+    [CHAIN.LINEA]: {
+      fetch,
+      start: "9-9-2025",
+    },
+    [CHAIN.MANTLE]: {
+      fetch,
+      start: "7-3-2025",
+    },
+    [CHAIN.MONAD]: {
+      fetch,
+      start: "11-19-2025",
+    },
+    [CHAIN.OPTIMISM]: {
+      fetch,
+      start: "10-16-2025",
+    },
+    [CHAIN.PLASMA]: {
+      fetch,
+      start: "9-30-2025",
+    },
+    [CHAIN.POLYGON]: {
+      fetch,
+      start: "9-26-2025",
+    },
+    [CHAIN.TAC]: {
+      fetch,
+      start: "7-9-2025",
+    },
+    [CHAIN.UNICHAIN]: {
+      fetch,
+      start: "4-22-2025",
+    },
+    [CHAIN.WC]: {
+      fetch,
+      start: "5-22-2025",
+    },
+  },
+  methodology: {
+    Fees: "Management and Performance fees set by Lagoon vaults are paid in the form of minted shares.",
+    Revenue: "A portion of the fees collected are sent to the protocol's fee receiver."
   }
 }
 
